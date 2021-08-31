16ead5537e06a854c59289b7b492ecb654b2b115
mtmazzo <74214745+mtmazzo@users.noreply.github.com>
2021-08-30 20:18:41 -0400

    Update fizzbuzz.js

---
+++ fizzbuzz.js
@@ -0,0 +1,15 @@
+function getFizzBuzz(n) {
+    var a = [];
+    var i = 1;
+    while(i <= n) {
+      if(i % 3 == 0 && i % 5 == 0) a.push("FizzBuzz");
+      else if(i % 3 == 0) a.push("Fizz");
+      else if(i % 5 == 0) a.push("Buzz");
+      else a.push(i);
+      i++
+    }
+    return a;
+}
+
+//Demonstrate
+console.log(getFizzBuzz(30));
